b165d53c4bbd059553357c19673533d84cad75e5
kostia <kozzztia@ukr.net>
2023-10-17 10:48:43 +0300

    new one

--- src/components/Footer/page.tsx
+++ src/components/Footer/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const page = ({className} :{className?: string}) => {
     return (
         <div className={className!}>
-            fotter mathfacka
+            <p>fotter mathfacka</p>
             <Link href="/" style={{color :"red"}}>back</Link>
         </div>
     );
